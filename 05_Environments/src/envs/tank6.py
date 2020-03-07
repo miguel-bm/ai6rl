87f6dfffdc8e9f0dfd58dd857bbcb1a4417d9048
Miguel Blanco Marcos <miguel.blanco.marcos@gmail.com>
2020-03-07 21:04:13 +0100

    Added collision checks to tanks

--- 05_Environments/src/envs/tank6.py
+++ 05_Environments/src/envs/tank6.py
@@ -27,18 +27,25 @@ class TankSaturdays(gym.Env):
         TANK_SIDE = 5
         GAS = 1000
         CARTRIDGE = 100
+        HP = 3
+        BULLET_SPEED = 3
+        N_WALLS = 10
+        WIDTH_WALLS = 2
+        LENGTH_WALLS = (5, 16)
 
         self.bf_side = BATTLEFIELD_SIDE
-        self.bf_size = np.array([BATTLEFIELD_SIDE, BATTLEFIELD_SIDE])
         self.tank_side = TANK_SIDE
-        self.tank_size = np.array([TANK_SIDE, TANK_SIDE])
-        self.pad = self.tank_side//2
-        self.bullet_speed = 3
-        self.n_walls = 10
-        self.width_walls = 2
-        self.length_walls = (5, 16)
         self.gas = GAS
         self.cartridge = CARTRIDGE
+        self.HP = HP
+        self.bullet_speed = BULLET_SPEED
+        self.n_walls = N_WALLS
+        self.width_walls = WIDTH_WALLS
+        self.length_walls = LENGTH_WALLS
+
+        self.bf_size = np.array([self.bf_side, self.bf_side])
+        self.tank_size = np.array([self.tank_side, self.tank_side])
+        self.pad = self.tank_side//2
 
         self.action_space = spaces.Discrete(5)
         self.observation_space = spaces.Discrete(np.prod(self.bf_size))
@@ -55,17 +62,20 @@ class TankSaturdays(gym.Env):
             "shoot_left": 8,
             }
 
+        self.bullet = recordclass('Bullet', ['x', 'y', 'dx', 'dy'])
+        self.wall = namedtuple('Wall', ['x0', 'y0', 'x1', 'y1'])
+        self.tank = recordclass('Tank', ['x', 'y', 'gas', 'cartridge', 'HP'])
         self.dv_tuple = namedtuple('Velocities', ['dx', 'dy'])
-        self.v_actions = defaultdict(lambda x: self.dv_tuple(0, 0))
+
+        self.v_actions = defaultdict(lambda: self.dv_tuple(0, 0))
         self.v_actions[1] = self.dv_tuple(0, -1)
         self.v_actions[2] = self.dv_tuple(1, 0)
         self.v_actions[3] = self.dv_tuple(0, 1)
         self.v_actions[4] = self.dv_tuple(-1, 0)
 
-        self.bullet = recordclass('Bullet', ['x', 'y', 'dx', 'dy'])
-        self.wall = namedtuple('Wall', ['x0', 'y0', 'x1', 'y1'])
-        self.tank = recordclass('Tank', ['x', 'y', 'gas', 'bullets'])
 
+        self.last_action_b = None
+        self.last_action_w = None
         self.seed()
         self.reset()
         self.viewer = None
@@ -75,44 +85,72 @@ class TankSaturdays(gym.Env):
             seed if seed is not None else np.random.seed())
         return [seed]
 
-    def step(self, action_1, action_2=None):
-        """
-        0: UP
-        1: RIGHT
-        2: DOWN
-        3: LEFT
-        """
+    def reset(self):
 
-        # move bullets
+        self.black = self.tank(self.pad, self.pad,
+            self.gas, self.cartridge, self.HP)
+        self.white = self.tank(self.bf_side-self.pad, self.bf_side-self.pad,
+            self.gas, self.cartridge, self.HP)
+
+        self.bullets = []
+
+        self.wall_m = np.zeros(self.bf_size)
+        self.walls = []
+        for _ in range(self.n_walls):
+            dir = self.np_random.randint(2)
+            length = self.np_random.randint(*self.length_walls)
+
+            x0 = self.np_random.randint(low = self.tank_side,
+                                    high = self.bf_side-self.tank_side-length)
+            x1 = x0 + length
+
+            y0 = self.np_random.randint(low = self.tank_side,
+                    high = self.bf_side-self.tank_side-self.width_walls)
+            y1 = y0 + self.width_walls
+
+            if dir == 1:
+                x0, x1, y0, y1 = y0, y1, x0, x1
+
+            self.walls.append(self.wall(x0, y0, x1, y1))
+            self.wall_m[x0:x1, y0:y1] = 1
+
+
+        self.state = None
+        return self._get_obs()
+
+
+    def step(self, action_b, action_w=None):
+
+        self.last_action_b = action_b
+        self.last_action_w = action_w
+
+        # Move bullets
         self._move_bullets()
 
-        # Check collisions between tanks and walls for given action
-        #action, action_2 = self._wall_collisions(action_1, action_2)
-        #
-        # self.black.x += self.v_actions[action_1].dx
-        # self.black.y += self.v_actions[action_1].dy
+        # Get tank displacement from actions
+        action_b_v = self.v_actions[action_b]
+        action_w_v = self.v_actions[action_w]
 
-        if action_1 == self.actions["idle"]:
-            pass
-        elif action_1 == self.actions["move_up"]:
-            self.black.y = max(self.pad, self.black.y-1)
-        elif action_1 == self.actions["move_right"]:
-            self.black.x = min(self.bf_side-self.pad, self.black.x+1)
-        elif action_1 == self.actions["move_down"]:
-            self.black.y = min(self.bf_side-self.pad, self.black.y+1)
-        elif action_1 == self.actions["move_left"]:
-            self.black.x = max(self.pad, self.black.x-1)
+        # Check wall collisions and update actions (only if tank tries to move)
+        if action_b_v != self.v_actions[0]:
+            action_b_v = self._wall_collisions(self.black, action_b_v)
+        if action_w_v != self.v_actions[0]:
+            action_w_v = self._wall_collisions(self.white, action_w_v)
 
-        if action_1 == self.actions["shoot_up"]:
+        # Move tanks
+        self.black.x += action_b_v.dx
+        self.black.y += action_b_v.dy
+
+        if action_b == self.actions["shoot_up"]:
             self.bullets.append(self.bullet(self.black.x, self.black.y-self.pad-1,
                                             0, -self.bullet_speed))
-        elif action_1 == self.actions["shoot_right"]:
+        elif action_b == self.actions["shoot_right"]:
             self.bullets.append(self.bullet(self.black.x+self.pad+1, self.black.y,
                                             self.bullet_speed, 0))
-        elif action_1 == self.actions["shoot_down"]:
+        elif action_b == self.actions["shoot_down"]:
             self.bullets.append(self.bullet(self.black.x, self.black.y+self.pad+1,
                                             0, self.bullet_speed))
-        elif action_1 == self.actions["shoot_left"]:
+        elif action_b == self.actions["shoot_left"]:
             self.bullets.append(self.bullet(self.black.x-self.pad-1, self.black.y,
                                             -self.bullet_speed, 0))
 
@@ -128,42 +166,6 @@ class TankSaturdays(gym.Env):
 
 
         return self._get_obs(), reward, done, {}
-
-    def reset(self):
-        #self.x = self.np_random.randint(low = self.pad,
-        #                                high = self.bf_side-self.pad)
-        #self.y = self.np_random.randint(low = self.pad,
-        #                                high = self.bf_side-self.pad)
-
-        self.black = self.tank(self.pad, self.pad, self.gas, self.cartridge)
-        self.white = self.tank(self.bf_side-self.pad, self.bf_side-self.pad,
-            self.gas, self.cartridge)
-
-        self.bullets = []
-
-        self.wall_m = np.zeros(self.bf_size)
-        self.walls = []
-        for _ in range(self.n_walls):
-            dir = self.np_random.randint(2)
-            len_ = self.np_random.randint(*self.length_walls)
-
-            x0 = self.np_random.randint(low = self.tank_side,
-                                    high = self.bf_side-self.tank_side-len_)
-            x1 = x0 + len_
-
-            y0 = self.np_random.randint(low = self.tank_side,
-                                    high = self.bf_side-self.tank_side-self.width_walls)
-            y1 = y0 + self.width_walls
-
-            if dir == 1:
-                x0, x1, y0, y1 = y0, y1, x0, x1
-
-            self.walls.append(self.wall(x0, y0, x1, y1))
-            self.wall_m[x0:x1, y0:y1] = 1
-
-
-        self.state = None
-        return self._get_obs()
 
     def _get_obs(self):
         return self.state
@@ -181,13 +183,28 @@ class TankSaturdays(gym.Env):
         self.bullets = list(filter(lambda b: (b.y>=0) and (b.y<self.bf_side), \
                                          self.bullets))
 
-    def _wall_collisions(self, tank, dx, dy):
-        tank_m = np.zeros(self.bf_size)
-        tank_m[tank.x-self.pad + dx:tank.x+self.pad+1 + dx,
-               tank.y-self.pad + dy:tank.y+self.pad+1 + dy] = 1
+    def _wall_collisions(self, tank, action_v):
+        dx = action_v.dx
+        dy = action_v.dy
 
+        # Make a matrix with ones in positions occupied by the tank after move
+        tank_m = np.zeros(self.bf_size + np.array([2,2]))
+        tank_m[tank.x+1-self.pad + dx:tank.x+1+self.pad+1 + dx,
+               tank.y+1-self.pad + dy:tank.y+1+self.pad+1 + dy] = 1
 
-        return None
+        # Make a matrix with ones in positions occupied by walls and borders
+        wall_m = np.zeros(self.bf_size + np.array([2,2]))
+        wall_m[[0,-1],:], wall_m[:, [0,-1]] = 1, 1  # Walls around battlefield
+        for wall in self.walls:
+            wall_m[wall.x0+1:wall.x1+1, wall.y0+1:wall.y1+1] = 1
+
+        # Multiply element-wise and sum
+        collisions = np.sum(wall_m * tank_m)
+
+        if collisions > 0:
+            return self.v_actions[0]  # Set action velocity to idle
+
+        return action_v
 
     def render(self, mode='console'):
 
@@ -211,7 +228,8 @@ class TankSaturdays(gym.Env):
 
             for row in self.render_m.tolist():
                 print(" ".join(row))
-            print(len(self.bullets))
+            if self.last_action_b is not None:
+                print(self.last_action_b)
             typer.clear()
 
     def get_keys_to_action(self):
